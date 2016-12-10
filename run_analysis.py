9629ecf62a67bd60be10458eeaabdc82bf88c29c
Tully Foote <tfoote@osrfoundation.org>
2016-12-09 17:33:33 -0800

    adding argument parsing

--- run_analysis.py
+++ run_analysis.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import subprocess
 import yaml
-
-
-
-
-agg_dir = 'aggrepo'
-output_dir = 'output'
-
-rosdistro = 'indigo'
-metapackage = 'ALL'
-
-#input_file = 'all.rosinstall'
-
 
 
 
@@ -29,41 +18,41 @@ def generate_rosinstall(rosdistro, metapackage='ALL'):
     return subprocess.check_output(cmd)
 
 
-def run(cmd, agg_dir):
-    print("Running [in directory %s] %s" % (agg_dir, ' '.join(cmd) ))
-    subprocess.check_call(cmd, cwd=agg_dir)
+def run(cmd, repo_dir):
+    print("Running [in directory %s] %s" % (repo_dir, ' '.join(cmd) ))
+    subprocess.check_call(cmd, cwd=repo_dir)
 
 
 def git_check_remote_url(repo_dir, name):
     cmd = ['git', 'config', 'remote.%s.url' % name]
     try:
-        return subprocess.check_output(cmd, cwd=agg_dir).rstrip()
+        return subprocess.check_output(cmd, cwd=repo_dir).rstrip()
     except subprocess.CalledProcessError as ex:
         return None
 
 
-def import_repo(name, branch, url, agg_dir):
-    first_time = not os.path.exists(os.path.join(agg_dir, name))
+def import_repo(name, branch, url, repo_dir):
+    first_time = not os.path.exists(os.path.join(repo_dir, name))
     if first_time:
         cmd = ['git', 'remote', 'add', name, url]
-        run(cmd, agg_dir)
+        run(cmd, repo_dir)
     else:
         # Check for a changed url
-        existing_url = git_check_remote_url(agg_dir, name)
+        existing_url = git_check_remote_url(repo_dir, name)
         assert existing_url
         if existing_url != url:
             cmd = ['git', 'remote', 'set-url', name, url]
-            run(cmd, agg_dir)
+            run(cmd, repo_dir)
     cmd = ['git', 'fetch', name]
-    run(cmd, agg_dir)
+    run(cmd, repo_dir)
     cmd = ['git', 'merge', '-s', 'ours', '--no-commit', '%s/%s' % (name, branch)]
-    run(cmd, agg_dir)
+    run(cmd, repo_dir)
     # The merge will find the prefix automatically after the first time
     if first_time:
         cmd = ['git', 'read-tree', '--prefix=%s/' % name, '-u', '%s/%s' % (name, branch)]
-        run(cmd, agg_dir)
+        run(cmd, repo_dir)
     cmd = ['git', 'commit', '-m', '"Imported %s as a subtree"' % name]
-    run(cmd, agg_dir)
+    run(cmd, repo_dir)
 
 
 def run_gitstats(repository_dir, output_dir):
@@ -75,46 +64,47 @@ def run_gitstats(repository_dir, output_dir):
     cmd = ['gitstats', repository_dir, gitstats_dir]
     subprocess.check_call(cmd)
 
-def run_cloc(agg_dir, output_dir):
-    cmd = ['cloc', agg_dir, '--out=' + os.path.join(output_dir, 'cloc.txt')]
+def run_cloc(repo_dir, output_dir):
+    cmd = ['cloc', repo_dir, '--out=' + os.path.join(output_dir, 'cloc.txt')]
     subprocess.check_call(cmd)
 
 
-def run_sloccount(agg_dir, output_dir):
+def run_sloccount(repo_dir, output_dir):
     sloccount_file = os.path.join(output_dir, 'sloccount.txt')
-    cmd = ['sloccount', agg_dir]
+    cmd = ['sloccount', repo_dir]
     output = subprocess.check_output(cmd)
     with open(sloccount_file, 'w') as fh:
         fh.write(output)
 
 
-def setup_aggregate_repo(agg_dir):
-    if not os.path.exists(agg_dir):
-        print("Setting up aggregate repo %s " % agg_dir)
-        os.makedirs(agg_dir)
+def setup_aggregate_repo(repo_dir):
+    if not os.path.exists(repo_dir):
+        print("Setting up aggregate repo %s " % repo_dir)
+        os.makedirs(repo_dir)
 
         cmd = ['git', 'init']
-        run(cmd, agg_dir)
+        run(cmd, repo_dir)
 
-        readme = os.path.join(agg_dir, 'README')
+        readme = os.path.join(repo_dir, 'README')
         with open(readme, 'w') as fh:
             fh.write("A directory into which to import ROS repos for statistical analysis")
         cmd = ['git', 'add', 'README']
-        run(cmd, agg_dir)
+        run(cmd, repo_dir)
         cmd = ['git', 'commit', '-m', '"A repo for ROS statistics"']
-        run(cmd, agg_dir)
+        run(cmd, repo_dir)
     else:
         print("Aggregate repo already exists, skipping setup")
 
 
-def main():
-    setup_aggregate_repo(agg_dir)
+def main(args):
+    setup_aggregate_repo(args.aggregate_repo_path)
 
-    # TODO arg parsing for custom rosinstall file
-    #with open(input_file, 'r') as fh:
-    #    yaml_file = fh.read()
-
-    yaml_file = generate_rosinstall(rosdistro)
+    if args.rosinstall_file:
+        with open(args.rosinstall_file, 'r') as fh:
+            yaml_file = fh.read()
+            print("Using passed rosinstall [%s] instead of generating" % args.rosinstall_file)
+    else:
+        yaml_file = generate_rosinstall(args.rosdistro, metapackage=args.metapackage)
 
     yd = yaml.load(yaml_file)
 
@@ -125,18 +115,40 @@ def main():
             continue
         entry = e['git']
         try:
-            import_repo(entry['local-name'], entry['version'], entry['uri'], agg_dir)
+            import_repo(entry['local-name'], entry['version'], entry['uri'], args.aggregate_repo_path)
         except subprocess.CalledProcessError as ex:
             errors[entry['local-name']] = str(ex)
 
 
-    run_gitstats(agg_dir, output_dir)
-    run_cloc(agg_dir, output_dir)
-    run_sloccount(agg_dir, output_dir)
+    run_gitstats(args.aggregate_repo_path, args.output_dir)
+    run_cloc(args.aggregate_repo_path, args.output_dir)
+    run_sloccount(args.aggregate_repo_path, args.output_dir)
 
     print("Errors encountered during cloning include")
     print(errors)
 
 
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser(description='Analyze rosdistros')
+    parser.add_argument('--rosdistro', default='indigo',
+                        help='The rosdistro to analyze')
+    parser.add_argument('--metapackage', default='ALL',
+                        help='The metapackage to analyze with all dependencies, or ALL')
+    parser.add_argument('--rosinstall-file', action='store',
+                        help='Use a rosinstall file passed instead of generating.')
+    parser.add_argument('--output-dir', default='output',
+                        help='The directory into which to output.')
+    parser.add_argument('--aggregate-repo-path', action='store',
+                        help=
+                        'Generate the aggregate repo into this path, '
+                        'default aggregate_<ROSDISTRO>_<METAPACKAGE>.')
+    args=parser.parse_args()
+
+    if args.rosinstall_file:
+        if not os.path.exists(args.rosinstall_file):
+            parser.error("rosinstall file passed does not exist")
+    if not args.aggregate_repo_path:
+        args.aggregate_repo_path = 'aggregate_%s_s%' % (args.rosdistro, args.metapackage)
+    
+    main(args)
