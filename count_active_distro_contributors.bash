7ff86fe4286ec72b4e474a22d6c67c6faaa892bd
Tully Foote <tfoote@osrfoundation.org>
2022-04-22 00:53:43 -0700

    finally able to get a full count

--- count_active_distro_contributors.bash
+++ count_active_distro_contributors.bash
@@ -12,13 +12,17 @@ OUTPUT_DIR=output_$AGG_REPO
 ./run_analysis.py --rosdistro noetic --aggregate-repo-path $AGG_REPO --no-analyze
 
 
-./run_analysis.py --aggregate-repo-path $AGG_REPO --analyze-only --output-dir $OUTPUT_DIR
+# Now crashing on full checkout
+#./run_analysis.py --aggregate-repo-path $AGG_REPO --analyze-only --output-dir $OUTPUT_DIR
 
 for YEAR in {2008..2022}
 do
 
-  docker run -v "${PWD}/$AGG_REPO:/repo" mergestat/mergestat summarize commits --start "$YEAR-01-01" --end "$YEAR-12-31" --json > $OUTPUT_DIR/mergestat_$YEAR.json
-
+  #docker run -v "${PWD}/$AGG_REPO:/repo" mergestat/mergestat summarize commits --start "$YEAR-01-01" --end "$YEAR-12-31" --json > $OUTPUT_DIR/mergestat_$YEAR.json
+  docker run -v "${PWD}/$AGG_REPO:/repo" mergestat/mergestat "SELECT commits.author_email from commits where commits.author_when >= '$YEAR-01-01' and commits.author_when <= '$YEAR-12-31'" -f csv-noheader | sort | uniq > $OUTPUT_DIR/unique_authors_$YEAR.txt
 done
 
-(cd $OUTPUT_DIR && python3 ../parse_authors_json.py)
+# Parse summary from mergestat
+#(cd $OUTPUT_DIR && python3 ../parse_authors_json.py)
+
+wc -l $OUTPUT_DIR/unique_authors_*
